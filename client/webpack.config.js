c90d1ab65a8050a3804520109b724e8d35fdaa88
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2022-10-29 08:23:15 +0300

    less loader init with antd

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,7 +38,28 @@ module.exports = {
       },
       {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+            options: {
+              lessOptions: {
+                // If you are using less-loader@5 please spread the lessOptions to options directly
+                modifyVars: {
+                  'primary-color': '#1DA57A',
+                  'link-color': '#1DA57A',
+                  'border-radius-base': '2px',
+                },
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
       },
     ],
   },
