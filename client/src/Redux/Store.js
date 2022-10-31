29b8d504afe3956e15442c6a28d1971ed4c842b5
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2022-10-31 17:20:13 +0300

    Auto model importer

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,8 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
-import Home from '../Page/Home/model';
-import Buy from '../Page/Buy/model';
-export const store = configureStore({
-  reducer: { Home, Buy },
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { Routers } from '../utilities/routes';
+
+let Reducers = [];
+Routers.map(({ tag, model }) => {
+  if (model) {
+    return Reducers.push({ [tag]: require('../Page/' + tag + '/model').default });
+  }
 });
-// Dispatcher(store);
+export const store = configureStore({
+  reducer: {
+    ...Reducers[0],
+    ...Reducers[1],
+    ...Reducers[2],
+    ...Reducers[3],
+    ...Reducers[4],
+    ...Reducers[5],
+    ...Reducers[6],
+    ...Reducers[7],
+    ...Reducers[8],
+    ...Reducers[9],
+    ...Reducers[10],
+    ...Reducers[11],
+    ...Reducers[12],
+    ...Reducers[13],
+    ...Reducers[14],
+    ...Reducers[15],
+    ...Reducers[16],
+    ...Reducers[17],
+    ...Reducers[18],
+    ...Reducers[19],
+    ...Reducers[20],
+    ...Reducers[21],
+    ...Reducers[22],
+    ...Reducers[23],
+    ...Reducers[24],
+    ...Reducers[25],
+    ...Reducers[26],
+  },
+});
 export default store;
