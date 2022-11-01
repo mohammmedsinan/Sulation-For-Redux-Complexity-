f503da055d061b5792c9062e6afc86e434eee5fa
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2022-11-01 11:41:14 +0300

    config file

---
+++ client/src/utilities/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  Site_Name: 'test',
+  Site_Logo: './',
+  Site_Copy_Right: '@test',
+  Api_Base_URl: 'http://localhost:3000',
+  Header_Fixed: false,
+  Site_Theme: {
+    Secondary: '#',
+    Primary: '#',
+    Links: '#',
+  },
+};
