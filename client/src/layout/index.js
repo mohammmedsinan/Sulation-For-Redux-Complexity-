5c04f32066d412c3d3639c6be9fa2c5c7fff1352
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-02-07 05:50:46 +0300

    custom Layout

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,14 +1,27 @@
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import { Routers } from '../routes';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import BaseLayout from './DashboardLayout';
 
 function index(props) {
   const state = useSelector((state) => state);
+  const location = useLocation();
+  const [switcher, setSwitcher] = React.useState('');
+  React.useEffect(() => {
+    if (location.pathname.split('/').length === 2) {
+      setSwitcher(location.pathname.split('/')[1].toLowerCase());
+    }
+    if (location.pathname.split('/').length >= 3) {
+      setSwitcher(location.pathname.split('/')[2].toLowerCase());
+    }
+  }, [location.pathname]);
   return (
-    <BaseLayout
-      child={Routers.map(({ name, pin, outSide, url, parentId }) => {
+    <>
+      {Routers.map(({ name, pin, outSide, url, parentId, customLayout }) => {
+        let CustomLayout = require(`./DashboardLayout`).default;
+        if (customLayout) {
+          CustomLayout = require(`./${customLayout}`).default;
+        }
         let AllRoutes = '';
         if (!pin) {
           if (!outSide) AllRoutes = require('../Page/' + name + '/index').default;
@@ -18,14 +31,26 @@ function index(props) {
             }/${name}`).default;
         }
         return (
-          <Route
-            path={url}
-            key={name}
-            element={<AllRoutes dispatch={props.Dispatch} state={state} />}
-          />
+          <>
+            {switcher === name.toLocaleLowerCase() && (
+              <CustomLayout
+                child={
+                  !customLayout ? (
+                    <Route
+                      path={url}
+                      key={name}
+                      element={<AllRoutes dispatch={props.Dispatch} state={state} />}
+                    />
+                  ) : (
+                    <AllRoutes dispatch={props.Dispatch} state={state} />
+                  )
+                }
+              />
+            )}
+          </>
         );
       })}
-    />
+    </>
   );
 }
 export default index;
