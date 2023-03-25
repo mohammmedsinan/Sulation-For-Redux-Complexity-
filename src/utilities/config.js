13b2311864d1283f4a93f581e7d720f9c458dbf6
mohammmedsinan <mohammedbusiness1099@gmail.com>
2023-03-25 14:08:47 +0300

    Extra Folders and pages

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  Site_Name: 'test',
-  Site_Logo: './',
-  Site_Copy_Right: '@test',
-  Api_Base_URl: 'http://192.168.1.242:5000/',
+  Site_Name: "test",
+  Site_Logo: "./",
+  Site_Copy_Right: "@test",
+  Api_Base_URl: "http://192.168.1.242:5000/",
   Header_Fixed: false,
   Site_Theme: {
     // '@primary-color': '#EBE2C7',
